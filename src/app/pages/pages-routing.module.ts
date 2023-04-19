09258ccbc283433cfb6995007a3cc8f956f871e9
IJAZ KHAN. S <ijaskhan4343@gmail.com>
2023-04-19 22:33:00 +0400

    new build 19/04

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -4,7 +4,8 @@ import { PagesComponent } from './pages.component';
 import { AutomotiveServiceComponent } from './automotive-service/automotive-service.component';
 import { CommercialServiceComponent } from './commercial-service/commercial-service.component';
 import { ResidentialServiceComponent } from './residential-service/residential-service.component';
-
+import { StoreDetailsComponent } from './store-details/store-details.component';
+import { StoreListComponent } from './store-list/store-list.component';
 const routes: Routes = [{ path: '', component: PagesComponent },
 {
   path:'automotive', component:AutomotiveServiceComponent
@@ -14,6 +15,12 @@ const routes: Routes = [{ path: '', component: PagesComponent },
 },
 {
   path: 'residential', component:ResidentialServiceComponent
+},
+{
+  path: 'store-details', component:StoreDetailsComponent
+},
+{
+  path: 'store-list', component:StoreListComponent
 }
 ];
 
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,6 +6,8 @@ import { PagesComponent } from './pages.component';
 import { ResidentialServiceComponent } from './residential-service/residential-service.component';
 import { CommercialServiceComponent } from './commercial-service/commercial-service.component';
 import { AutomotiveServiceComponent } from './automotive-service/automotive-service.component';
+import { StoreDetailsComponent } from './store-details/store-details.component';
+import { StoreListComponent } from './store-list/store-list.component';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { AutomotiveServiceComponent } from './automotive-service/automotive-serv
     PagesComponent,
     ResidentialServiceComponent,
     CommercialServiceComponent,
-    AutomotiveServiceComponent
+    AutomotiveServiceComponent,
+    StoreDetailsComponent,
+    StoreListComponent
   ],
   imports: [
     CommonModule,
